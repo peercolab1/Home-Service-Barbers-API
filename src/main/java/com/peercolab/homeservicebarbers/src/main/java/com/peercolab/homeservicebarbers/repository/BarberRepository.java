1ebb22bf621b4f38465290299fadd0b77bd77963
Anakhe Ajayi <93340132+AAnakhe@users.noreply.github.com>
2022-06-15 13:43:53 -0700

    Updated
    Errors in package and class names fixed

---
+++ src/main/java/com/peercolab/homeservicebarbers/src/main/java/com/peercolab/homeservicebarbers/repository/BarberRepository.java
@@ -0,0 +1,7 @@
+package com.peercolab.homeservicebarbers.repository;
+
+import com.peercolab.homeservicebarbers.entity.Barber;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BarberRepository extends JpaRepository<Barber, Integer> {
+}
