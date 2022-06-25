ba5a7b8302b0418b8326eb0452e32864d9ea9205
Anakhe Ajayi <93340132+AAnakhe@users.noreply.github.com>
2022-06-25 05:31:33 -0700

    Home-Service-Barbers-API/updated
    Business logic was moved from BarberController.java to BarberService.java

--- src/main/java/com/peercolab/homeservicebarbers/controller/BarberController.java
+++ src/main/java/com/peercolab/homeservicebarbers/controller/BarberController.java
@@ -1,11 +1,9 @@
 package com.peercolab.homeservicebarbers.controller;
 
 import com.peercolab.homeservicebarbers.dto.BarberDTO;
-import com.peercolab.homeservicebarbers.mapper.BarberMapper;
-import com.peercolab.homeservicebarbers.model.Barber;
-import com.peercolab.homeservicebarbers.repository.BarberRepository;
+import com.peercolab.homeservicebarbers.entity.Barber;
+import com.peercolab.homeservicebarbers.service.BarberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,32 +13,25 @@ import java.util.List;
 public class BarberController {
 
     @Autowired
-    private BarberMapper barberMapper;
-
-    @Autowired
-    private BarberRepository barberRepository;
+    private BarberService barberService;
 
     @PostMapping("/barbers")
     public ResponseEntity<Barber> save(@RequestBody BarberDTO barberDTO) {
-        return new ResponseEntity<>(barberRepository.save(
-                barberMapper.dtoToModel(barberDTO)), HttpStatus.CREATED);
+        return barberService.saveBarberInfo(barberDTO);
     }
 
     @GetMapping("/barbers")
     public ResponseEntity<List<BarberDTO>> findAll(){
-        return new ResponseEntity<>(barberMapper.modelsToDtos(barberRepository.findAll()), HttpStatus.OK);
+        return barberService.getBarbersInfo();
     }
 
     @GetMapping("/barbers/{id}")
     public ResponseEntity<BarberDTO> findById(@PathVariable(value = "id") Integer id){
-        return new ResponseEntity<>(barberMapper.modelToDto(barberRepository.findById(id).get()), HttpStatus.OK);
+        return barberService.getInfoById(id);
     }
 
     @DeleteMapping("/barbers/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable(value = "id") Integer id){
-        BarberDTO barberDTO = barberMapper.modelToDto(barberRepository.findById(id).get());
-
-        barberRepository.deleteById(barberDTO.getBarber_id());
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return barberService.deleteInfoById(id);
     }
 }
--- src/main/java/com/peercolab/homeservicebarbers/service/BarberService.java
+++ src/main/java/com/peercolab/homeservicebarbers/service/BarberService.java
@@ -1,42 +1,39 @@
 package com.peercolab.homeservicebarbers.service;
 
 import com.peercolab.homeservicebarbers.dto.BarberDTO;
-import com.peercolab.homeservicebarbers.mapper.BarberMapper;
 import com.peercolab.homeservicebarbers.entity.Barber;
+import com.peercolab.homeservicebarbers.mapper.BarberMapper;
 import com.peercolab.homeservicebarbers.repository.BarberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RestController
+@Service
 public class BarberService {
+
     @Autowired
     private BarberMapper barberMapper;
 
     @Autowired
     private BarberRepository barberRepository;
 
-    @PostMapping("/barbers")
-    public ResponseEntity<Barber> save(@RequestBody BarberDTO barberDTO) {
+    public ResponseEntity<Barber> saveBarberInfo(BarberDTO barberDTO) {
         return new ResponseEntity<>(barberRepository.save(
                 barberMapper.dtoToModel(barberDTO)), HttpStatus.CREATED);
     }
 
-    @GetMapping("/barbers")
-    public ResponseEntity<List<BarberDTO>> findAll(){
+    public ResponseEntity<List<BarberDTO>> getBarbersInfo(){
         return new ResponseEntity<>(barberMapper.modelsToDtos(barberRepository.findAll()), HttpStatus.OK);
     }
 
-    @GetMapping("/barbers/{id}")
-    public ResponseEntity<BarberDTO> findById(@PathVariable(value = "id") Integer id){
+    public ResponseEntity<BarberDTO> getInfoById(Integer id){
         return new ResponseEntity<>(barberMapper.modelToDto(barberRepository.findById(id).get()), HttpStatus.OK);
     }
 
-    @DeleteMapping("/barbers/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable(value = "id") Integer id){
+    public ResponseEntity<Void> deleteInfoById(Integer id){
         BarberDTO barberDTO = barberMapper.modelToDto(barberRepository.findById(id).get());
 
         barberRepository.deleteById(barberDTO.getBarber_id());
