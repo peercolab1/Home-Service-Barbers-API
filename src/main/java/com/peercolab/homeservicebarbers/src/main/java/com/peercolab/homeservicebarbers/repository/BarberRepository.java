0ccec2f9055b43a684c1f6a821a45c6a777f955f
zedbeit <devbeita@gmail.com>
2022-06-28 12:30:19 +0100

    fix sme changes

--- src/main/java/com/peercolab/homeservicebarbers/src/main/java/com/peercolab/homeservicebarbers/repository/BarberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.peercolab.homeservicebarbers.repository;
-
-import com.peercolab.homeservicebarbers.entity.Barber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarberRepository extends JpaRepository<Barber, Integer> {
-}
